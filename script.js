88e7786feb820591a3f7cdc18d05432cf3fc28d9
William Zheng <williamzheng2001@gmail.com>
2022-06-01 20:46:06 -0400

    Create module for game board

---
+++ script.js
@@ -0,0 +1,26 @@
+const GameBoard = (function() {
+    let board = [
+        [null, null, null],
+        [null, null, null],
+        [null, null, null]
+    ];
+    const clearBoard = () => {
+        // TODO
+    };
+    const placeX = (r, c) => {
+        // TODO
+    };
+    const placeO = (r, c) => {
+        // TODO
+    };
+    const checkWin = () => {
+        // TODO
+    };
+    return {
+        board,
+        clearBoard,
+        placeX,
+        placeO,
+        checkWin
+    };
+})();
