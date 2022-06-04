da33608b1964fc2f975a10ce8e7ce9141379c865
William Zheng <williamzheng2001@gmail.com>
2022-06-04 18:22:25 -0400

    Clean up code

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-const GameBoard = (function() {
-    let board = [
+const gameBoard = (function() {
+    let _board = [
         [null, null, null],
         [null, null, null],
         [null, null, null]
     ];
 
     const getBoard = () => {
-        return board;
+        return _board;
     }
 
     const clearBoard = () => {
-        board = [
+        _board = [
             [null, null, null],
             [null, null, null],
             [null, null, null]
@@ -18,29 +18,29 @@ const GameBoard = (function() {
     };
 
     const isEmptySpot = (r, c) => {
-        return board[r][c] === null;
+        return _board[r][c] === null;
     };
 
     const placeMarker = (r, c, marker) => {
-        board[r][c] = marker;
+        _board[r][c] = marker;
     };
 
     const checkStatus = (p1, p2) => {
-        const rowStatus = checkRows(p1, p1);
-        const colStatus = checkColumns(p1, p2);
-        const diagonalStatus = checkDiagonals(p1, p2);
+        const rowStatus = _checkRows(p1, p1);
+        const colStatus = _checkColumns(p1, p2);
+        const diagonalStatus = _checkDiagonals(p1, p2);
 
         if (rowStatus !== null) return rowStatus;
         else if (colStatus !== null) return colStatus;
         else if (diagonalStatus !== null) return diagonalStatus;
-        else if (!emptySpots()) return 'Tie';
+        else if (!_emptySpots()) return 'Tie';
         else return null;
     };
 
-    const checkRows = (p1, p2) => {
+    const _checkRows = (p1, p2) => {
         for (let r = 0; r < 3; r++) {
-            if (board[r][0] !== null && board[r][0] === board[r][1] && board[r][1] === board[r][2]) {
-                if (board[r][0] === p1.marker) return p1;
+            if (_board[r][0] !== null && _board[r][0] === _board[r][1] && _board[r][1] === _board[r][2]) {
+                if (_board[r][0] === p1.marker) return p1;
                 else return p2;
             }
         }
@@ -48,10 +48,10 @@ const GameBoard = (function() {
         return null;
     };
 
-    const checkColumns = (p1, p2) => {
+    const _checkColumns = (p1, p2) => {
         for (let c = 0; c < 3; c++) {
-            if (board[0][c] !== null && board[0][c] === board[1][c] && board[1][c] === board[2][c]) {
-                if (board[0][c] === p1.marker) return p1;
+            if (_board[0][c] !== null && _board[0][c] === _board[1][c] && _board[1][c] === _board[2][c]) {
+                if (_board[0][c] === p1.marker) return p1;
                 else return p2;
             }
         }
@@ -59,26 +59,26 @@ const GameBoard = (function() {
         return null;
     };
 
-    const checkDiagonals = (p1, p2) => {
-        if (board[0][0] !== null && board[0][0] === board[1][1] && board[1][1] === board[2][2]) {
-            if (board[0][0] === p1.marker) return p1;
+    const _checkDiagonals = (p1, p2) => {
+        if (_board[0][0] !== null && _board[0][0] === _board[1][1] && _board[1][1] === _board[2][2]) {
+            if (_board[0][0] === p1.marker) return p1;
             else return p2;
         }
 
-        if (board[0][2] !== null && board[0][2] === board[1][1] && board[1][1] === board[2][0]) {
-            if (board[0][2] === p1.marker) return p1;
+        if (_board[0][2] !== null && _board[0][2] === _board[1][1] && _board[1][1] === _board[2][0]) {
+            if (_board[0][2] === p1.marker) return p1;
             else return p2;
         }
 
         return null;
     };
 
-    const emptySpots = () => {
+    const _emptySpots = () => {
         let count = 0;
 
         for (let r = 0; r < 3; r++) {
             for (let c = 0; c < 3; c++) {
-                if (board[r][c] === null) count++;
+                if (_board[r][c] === null) count++;
             }
         }
 
