bcacbf77c63975e97745300541693a0e6edd91c9
William Zheng <williamzheng2001@gmail.com>
2022-06-04 18:10:40 -0400

    Break function to check game status into multiple functions

--- script.js
+++ script.js
@@ -4,9 +4,11 @@ const GameBoard = (function() {
         [null, null, null],
         [null, null, null]
     ];
+
     const getBoard = () => {
         return board;
     }
+
     const clearBoard = () => {
         board = [
             [null, null, null],
@@ -14,53 +16,75 @@ const GameBoard = (function() {
             [null, null, null]
         ];
     };
+
     const isEmptySpot = (r, c) => {
         return board[r][c] === null;
     };
+
     const placeMarker = (r, c, marker) => {
         board[r][c] = marker;
     };
-    const checkStatus = (player1, player2) => {
-        // Check rows and columns
 
+    const checkStatus = (p1, p2) => {
+        const rowStatus = checkRows(p1, p1);
+        const colStatus = checkColumns(p1, p2);
+        const diagonalStatus = checkDiagonals(p1, p2);
+
+        if (rowStatus !== null) return rowStatus;
+        else if (colStatus !== null) return colStatus;
+        else if (diagonalStatus !== null) return diagonalStatus;
+        else if (!emptySpots()) return 'Tie';
+        else return null;
+    };
+
+    const checkRows = (p1, p2) => {
         for (let r = 0; r < 3; r++) {
             if (board[r][0] !== null && board[r][0] === board[r][1] && board[r][1] === board[r][2]) {
-                if (board[r][0] === player1.marker) return player1.marker;
-                else return player2.marker;
+                if (board[r][0] === p1.marker) return p1;
+                else return p2;
             }
         }
 
+        return null;
+    };
+
+    const checkColumns = (p1, p2) => {
         for (let c = 0; c < 3; c++) {
             if (board[0][c] !== null && board[0][c] === board[1][c] && board[1][c] === board[2][c]) {
-                if (board[0][c] === player1.marker) return player1.marker;
-                else return player2.marker;
+                if (board[0][c] === p1.marker) return p1;
+                else return p2;
             }
         }
 
-        // Check diagonals
+        return null;
+    };
 
+    const checkDiagonals = (p1, p2) => {
         if (board[0][0] !== null && board[0][0] === board[1][1] && board[1][1] === board[2][2]) {
-            if (board[0][0] === player1.marker) return player1.marker;
-            else return player2.marker;
+            if (board[0][0] === p1.marker) return p1;
+            else return p2;
         }
 
         if (board[0][2] !== null && board[0][2] === board[1][1] && board[1][1] === board[2][0]) {
-            if (board[0][2] === player1.marker) return player1.marker;
-            else return player2.marker;
+            if (board[0][2] === p1.marker) return p1;
+            else return p2;
         }
 
-        // Check for tie
+        return null;
+    };
 
-        let emptySpots = 0;
+    const emptySpots = () => {
+        let count = 0;
+
         for (let r = 0; r < 3; r++) {
             for (let c = 0; c < 3; c++) {
-                if (board[r][c] === null) emptySpots++;
+                if (board[r][c] === null) count++;
             }
         }
-        if (emptySpots === 0) return 'Tie';
-        
-        return null; // Game in progress
+
+        return count;
     };
+
     return {
         getBoard,
         clearBoard,
